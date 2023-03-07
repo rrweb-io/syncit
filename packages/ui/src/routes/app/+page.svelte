73e196e2c6c28c96fa833fef117795e9a3bd0a3c
Justin Halsall <Juice10@users.noreply.github.com>
2023-03-07 14:42:43 +0100

    Migrate @syncit/ui to rrweb 2.0

---
+++ packages/ui/src/routes/app/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="typescript">
+	import * as syncit from '$lib/index';
+	import * as syncitTransporter from '@syncit/transporter';
+
+	new syncit.App({
+		target: document.body,
+		props: {
+			bufferMs: 100,
+			createTransporter({ role, uid }) {
+				return new syncitTransporter.LocalStorageTransporter();
+			}
+		}
+	});
+</script>
